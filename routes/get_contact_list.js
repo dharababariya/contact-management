e039e4c746ddbb411dafe5c00fc1f0c146d8bad7
Dhara <dharababariya34@gmail.com>
2019-04-29 12:51:55 +0530

    setup heroku

--- routes/get_contact_list.js
+++ routes/get_contact_list.js
@@ -5,29 +5,17 @@ const router = express.Router();
 const knex = require('../helpers/knex');
 
 // contact list
+const contact_list = async(req, res, next) => {
 
+    const result = await knex("public.contact_list").select("*")
 
-    const contact_list = async( req,res,next)=>{
+    res
+        .status(200)
+        .send({message: "get all contact", result: result})
 
-        const result = await knex("public.contact_list").select("*")
-    
-        res
-        .status(201)
-        .send({
-            message:"get all contact",
-            result: result
-        })
-    
-    }
-    
-
-
-
-
+}
 
 //get contact list api
 router.get('/v1/api/get_contact_list', contact_list);
-
-
 
 module.exports = router;
