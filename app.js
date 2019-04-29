b17d7e26276d8ee9892b7898b68cd3dc6c5127fe
Dhara <dharababariya34@gmail.com>
2019-04-29 11:09:57 +0530

    get all contact

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 //import module
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const body_parser = require('body-parser');
+
+//middleware
+app.use(body_parser.json());
+
+
+//env config
+dotenv.config();
 
 //fire routes
 app.use(require('./routes/get_contact_list'));
--- routes/get_contact_list.js
+++ routes/get_contact_list.js
@@ -2,15 +2,31 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../helpers/knex');
 
 // contact list
-const contact_list = ( req,res,next)=>{
-    res.send('success');
 
-}
+
+    const contact_list = async( req,res,next)=>{
+
+        const result = await knex("public.contact_list").select("*")
+    
+        res
+        .status(201)
+        .send({
+            message:"get all contact",
+            result: result
+        })
+    
+    }
+    
+
+
+
+
 
 //get contact list api
-router.get('/v1/api/get_contact_list' , contact_list );
+router.get('/v1/api/get_contact_list', contact_list);
 
 
 
