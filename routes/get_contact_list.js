d98d5040aca727a7a399b8699bb46798692e1ca3
Dhara <dharababariya34@gmail.com>
2019-04-29 18:09:12 +0530

    change file

--- routes/get_contact_list.js
+++ routes/get_contact_list.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
-const knex = require('../helpers/knex');
+const knex = require('../db/knex');
 
 // contact list
 const contact_list = async(req, res, next) => {
 
-    const result = await knex("public.contact_list").select("*")
+    const result = await knex("public.contact_list").select("*");
+    console.log(result);
 
     res
         .status(200)
