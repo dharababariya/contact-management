5dc9fb8dbbd86deb9923e37e9082f1c71afcf615
Dhara <dharababariya34@gmail.com>
2019-04-30 19:00:46 +0530

    upadate and delete api

--- routes/delete_contact.js
+++ routes/delete_contact.js
@@ -2,27 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+
 const knex = require('../db/knex');
 
 // contact list
-const update_contact_list = async(req, res, next) => {
+const delete_contact_list = async(req, res, next) => {
+    
 
     try {
         const result = await knex("public.contact_list")
-            .update("name", req.body.name)
-            .where("id", "=", req.body.id).returning("*")
+            .delete()
+            .where("id", "=", req.query.id)
         return res
             .status(200)
-            .send({status: 'SUCCESS', data: result});
+            .send({ status: 'Successfully Deteted' });
     } catch (error) {
         console.error(error.message);
         res.status(500);
- 
-    }
-    
 
+    }
+ 
 }
 
 //get contact list api
-router.put('/api/update_contact_list', update_contact_list);
+router.delete('/api/delete_contact_list', delete_contact_list);
+
 module.exports = router;
